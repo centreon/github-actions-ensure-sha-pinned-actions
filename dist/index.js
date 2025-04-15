c555d9c1404ff28ee323d8eb1032526e599d243a
schapron <schapron@centreon.com>
2025-04-15 17:19:36 +0200

    debug

--- dist/index.js
+++ dist/index.js
@@ -38656,6 +38656,12 @@ async function run() {
       core.info('steps = ' + steps);
       core.info('workflowsPath = ' + workflowsPath);
 
+      yamlContents.forEach(function(item, index, arr) {
+        core.info('item = ' + item);
+        core.info('index = ' + index);
+        core.info('arr = ' + arr);
+      });
+
       if (steps === undefined) {
         core.setFailed(`The "${file}" pipeline does not contain any step.`);
       }
